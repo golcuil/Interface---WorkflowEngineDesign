637579bcc6720107a1849e0ce8a5df7c19b7a274
İlkin Berkay Gölcü <75281929+golcuil@users.noreply.github.com>
2022-02-22 12:37:45 +0300

    Workflow.cs

---
+++ Workflow.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Workflow : IWorkflow
+{
+    private readonly List<ITask> _tasks;
+
+    public Workflow()
+    {
+        _tasks = new List<ITask>();
+    }
+
+    public void Add(ITask task)
+    {
+        _tasks.Add(task);
+    }
+
+    public void Remove(ITask task)
+    {
+        _tasks.Remove(task);
+    }
+
+    public IEnumerable<ITask> GetTasks()
+    {
+        return _tasks;
+    }
+}
+
